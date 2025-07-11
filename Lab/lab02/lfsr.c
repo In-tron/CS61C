33f3c659799ea7829228c8ad6de838bf1480e105
Intron <johnnsqkxfsg@gmail.com>
2025-07-11 18:30:13 -0400

    Completed lab02.

--- Lab/lab02/lfsr.c
+++ Lab/lab02/lfsr.c
@@ -6,5 +6,6 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t bit = ((*reg >> 0) ^ (*reg >> 2) ^ (*reg >> 3) ^ (*reg >> 5)) & 1;
+    *reg = (*reg >> 1) | (bit << 15);
 }
-
